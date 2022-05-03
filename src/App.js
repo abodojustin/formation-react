483a606873d2e064e218c5597bfdf0baa7fbb1a1
jafacorp <56782606+abodojustin@users.noreply.github.com>
2022-05-03 06:25:34 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import DataProvider from "./DataProvider";
+
+const list = [
+  {
+    id: 1,
+    text: "learn React",
+    completed: false,
+  },
+  {
+    id: 2,
+    text: "pay Bills",
+    completed: false,
+  },
+  {
+    id: 3,
+    text: "buy Groceries",
+    completed: false,
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataProvider
+      items={list}
+      render={({ data, handleOnChange, handleOnSubmit }) => {
+        return (
+          <>
+            <form onSubmit={handleOnSubmit}>
+              <input type="text" onChange={handleOnChange} /> &nbsp;{" "}
+              <button type="submit">add</button>
+            </form>
+            <ul>
+              {data.todos.map((todo) => {
+                return <li key={todo.id}>{todo.text}</li>;
+              })}
+            </ul>
+          </>
+        );
+      }}
+    />
   );
 }
 
